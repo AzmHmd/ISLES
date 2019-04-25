48bdfcd0e7c14c24945209ad024366e7434129fc
Azam <AzmHmd@users.noreply.github.com>
2019-04-25 19:47:24 +0100

    Update read_files.py

--- read_files.py
+++ read_files.py
@@ -3,15 +3,26 @@
 import numpy as np
 import glob
 import nibabel as nib
-from nilearn import image
-from nilearn import plotting
+from nilearn import image, plotting
+import matplotlib
+%matplotlib inline
 
 path_to_read_files = '/Volumes/Seagate Backup Plus Drive/Azam/ISLES/2018/TRAINING/'
-files = [f for f in glob.glob(path_to_read_files+'*/*/*.nii')]
-for f in files:
-    if f.find("4DPWI")==-1:  # to make sure it is 3D data
-        print(f)
-        CT_img = nib.load(f)
-        print('image shape is : '+ str(CT_img.shape))
-        plotting.plot_img(CT_img)
-        plotting.show()
+cases = [f for f in glob.glob(path_to_read_files+'*')]
+for case in cases:
+    
+    print('*************  ' + case.split("/")[-1] + '  *****************')
+    files = [f for f in glob.glob(case+'/*/*.nii')]
+    for f in files:
+        if f.find("4DPWI")==-1:
+            CT_img = nib.load(f)
+            print('image shape is : '+ str(CT_img.shape))
+            img_data = CT_img.get_fdata()
+            print('maximum value of image intensity is: '+ str(img_data.max()))
+            print('type of data : ') 
+            print(CT_img.get_data_dtype())
+
+            plotting.plot_img(CT_img)
+            plotting.show()
+    print('---------------------------------------------------------------------------')
+
